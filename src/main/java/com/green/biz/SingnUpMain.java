e7594fa319c81019087b0f571d91eee472216b16
KimJunHwan <kdju203@naver.com>
2021-08-10 11:51:46 +0900

    회원가입 기능 완료

---
+++ src/main/java/com/green/biz/SingnUpMain.java
@@ -0,0 +1,22 @@
+package com.green.biz;
+
+public class SingnUpMain {
+	private String username;
+	private String password;
+	/*
+	 * 회원정보 클래스
+	 */
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
